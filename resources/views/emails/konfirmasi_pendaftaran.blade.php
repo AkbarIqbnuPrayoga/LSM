101108ee34af7c6c2810989e6496b9d22bff0999
unknown <prayogaiqbnu@gmail.com>
2025-05-21 01:32:49 +0700

    pembayaran pelatihan

---
+++ resources/views/emails/konfirmasi_pendaftaran.blade.php
@@ -0,0 +1,10 @@
+<h2>Terima kasih telah mendaftar Pelatihan: {{ $data['pelatihan'] }}!</h2>
+<p>Berikut adalah informasi pendaftaran Anda:</p>
+<ul>
+    <li><strong>Nama:</strong> {{ $data['nama_lengkap'] }}</li>
+    <li><strong>Email:</strong> {{ $data['email'] }}</li>
+    <li><strong>No. Telepon:</strong> {{ $data['no_telp'] }}</li>
+    <li><strong>Instansi:</strong> {{ $data['instansi'] }}</li>
+    <li><strong>Pelatihan:</strong> {{ $data['pelatihan'] }}</li>
+</ul>
+<p>Silakan lakukan pembayaran ke rekening berikut: <br><strong>123456789 - BNI a.n Panitia Pelatihan</strong></p>
